fb335a90371b692757dd62b67bae0dab9ad6d72d
akashthanniyar <akashremesh@gmail.com>
2022-12-06 16:47:28 +0530

    Creating Nodes and Modifying Dom

--- dom.js
+++ dom.js
@@ -44,13 +44,91 @@
 // var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
 // thirdItem.style.visibility = "hidden";  
 
-//Using QuerySelectorALL
-var secItem= document.querySelectorAll('.list-group-item');
-secItem[2].style.color="green";
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++) {
-    odd[i].style.backgroundColor="green";
-}
+// //Using QuerySelectorALL
+// var secItem= document.querySelectorAll('.list-group-item');
+// secItem[2].style.color="green";
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0;i<odd.length;i++) {
+//     odd[i].style.backgroundColor="green";
+// }
+
+// // traverse the DOM 
+// var itemList =document.querySelector('#items');
+// //parent Node 
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor="yellow";
+// console.log(itemList.parentNode.parentNode);
+// //parentelement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor="yellow";
+// console.log(itemList.parentElement.parentElement); 
+
+// //childNode
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor="green";
+
+// //firstChild
+// console.log(itemList.firstChild);
+
+// //firstelementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent="hello";
+
+// //lastChild
+// console.log(itemList.lastChild);
+
+// //lastelementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent="hi";
+
+// //nextSibling
+// console.log(itemList.nextSibling);
+
+// //nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// //previousSibling
+
+// console.log(itemList.previousSibling);
+
+// //previousElementSibing
+// console.log(itemList.previousElementSibling);
+
+//creating DOM element
+  // create a div 
+var newDiv = document.createElement('div');
+//Add CLass
+newDiv.className="hello"; 
+//add ID
+newDiv.id="hello1";
+//add Attribute 
+newDiv.setAttribute('title','Hello Div'); 
+
+//create text Node 
+ 
+var newDivtxt  = document.createTextNode('helloworld'); 
+
+// add text to div (append)
+
+newDiv.appendChild(newDivtxt);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+
+var h2 = document.querySelector('#items');
+var firstItem = document.querySelector('.list-group-item:nth-child(1)');
+h2.insertBefore(newDiv,firstItem);
+
+
+
+
+
+
+
+
 
 
 
