487ddb790b594cc8a3339339530fc9962f299aa2
akashthanniyar <akashremesh@gmail.com>
2022-12-06 13:14:15 +0530

    querySelector

--- dom.js
+++ dom.js
@@ -8,26 +8,52 @@
 
 
 
-var addItem = document.getElementsByClassName('title');
+// var addItem = document.getElementsByClassName('title');
 
-addItem[0].style.fontWeight="bold"; 
+// addItem[0].style.fontWeight="bold"; 
 
-addItem[0].style.color ="green";
+// addItem[0].style.color ="green";
 
-var items = document.getElementsByClassName("list-group-item");
-console.log(items[2]);
-items[2].style.backgroundColor="green";
+// var items = document.getElementsByClassName("list-group-item");
+// console.log(items[2]);
+// items[2].style.backgroundColor="green";
 
 
-for(var i=0;i<items.length;i++) {
-    items[i].style.fontWeight="bold";
-    items[i].style.color="orange";
-    items[i].style.backgroundColor="yellow";
+// for(var i=0;i<items.length;i++) {
+//     items[i].style.fontWeight="bold";
+//     items[i].style.color="orange";
+//     items[i].style.backgroundColor="yellow";
+// }
+// // get elements by id
+
+// var li = document.getElementsByTagName("li");
+// li[4].style.fontWeight="bold";
+// li[4].style.color="blue";
+
+// Query Selector 
+
+// var header = document.querySelector('#main-header');
+// console.log(header);
+// header.style.borderBottom="solid 10px #ccc";
+
+// var input = document.querySelector('input');
+// input.value="Hello World!";
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor="green";
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility = "hidden";  
+
+//Using QuerySelectorALL
+var secItem= document.querySelectorAll('.list-group-item');
+secItem[2].style.color="green";
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++) {
+    odd[i].style.backgroundColor="green";
 }
-// get elements by id
 
-var li = document.getElementsByTagName("li");
-li[4].style.fontWeight="bold";
-li[4].style.color="blue";
+
+
+
 
 
