2ce444125c7deab3c7ed46ab8766adcc477bae21
akashthanniyar <akashremesh@gmail.com>
2022-12-06 12:36:48 +0530

    getelebyTagname

--- dom.js
+++ dom.js
@@ -18,8 +18,16 @@ var items = document.getElementsByClassName("list-group-item");
 console.log(items[2]);
 items[2].style.backgroundColor="green";
 
+
 for(var i=0;i<items.length;i++) {
     items[i].style.fontWeight="bold";
     items[i].style.color="orange";
+    items[i].style.backgroundColor="yellow";
 }
+// get elements by id
+
+var li = document.getElementsByTagName("li");
+li[4].style.fontWeight="bold";
+li[4].style.color="blue";
+
 
