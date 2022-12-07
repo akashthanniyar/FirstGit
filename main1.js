1b5148b6f829bba9fc8a4bc1d64d6bc7c8980a61
akashthanniyar <akashremesh@gmail.com>
2022-12-07 14:52:13 +0530

    local storage

---
+++ main1.js
@@ -0,0 +1,48 @@
+// const ul = document.querySelector('.items');
+// ul.firstElementChild.textContent='Hello';
+// ul.firstElementChild.style.background = "green";
+// ul.children[1].style.background="yellow";
+// const btn = document.querySelector('.btn')
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector('.btn').style.background="green";
+// });
+// btn.addEventListener('mouseover', (e)=> {
+//     e.preventDefault();
+//     document.querySelector('body').classList.add('bg-dark');
+// });
+// btn.addEventListener('mouseout', (e)=> {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background="#ccc";
+// });
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit',onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    if(nameInput.value ==="" || emailInput.value==="") {
+        msg.classList.add('error');
+        msg.innerHTML='Please Enter Data';
+    }else {
+       const li = document.createElement('li');
+       li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+       userList.appendChild(li);
+
+       localStorage.setItem('Name', nameInput.value);
+       localStorage.setItem('mail', emailInput.value);
+       
+       nameInput.value='';
+       emailInput.value='';
+    }
+}
+
+
+
+
+ 
