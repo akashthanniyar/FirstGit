7560450f908fcce9994f54795751c950737be2e6
akashthanniyar <akashremesh@gmail.com>
2022-12-07 11:41:53 +0530

    Filter

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 form.addEventListener('submit' , addItem);
 
 itemList.addEventListener('click', removeItem);
 
+filter.addEventListener('keyup', filterItems);
+
 function addItem(e) {
     e.preventDefault();
     var newItem = document.getElementById('item').value;
+    var descp = document.getElementById('item1').value;
     var li=document.createElement('li');
     li.className='list-group-item';
     li.appendChild(document.createTextNode(newItem));
-    var editBtn = document.createElement('button');
-    editBtn.className="btn btn-sm float-right edit";
-    editBtn.appendChild(document.createTextNode('Edit'));
-    li.appendChild(editBtn);
-    itemList.appendChild(li);
+    li.appendChild(document.createTextNode(" " + descp));
+    //del button
     var delbtn = document.createElement('button');
     delbtn.className="btn btn-danger btn-sm float-right delete";
     delbtn.appendChild(document.createTextNode('X'));
     li.appendChild(delbtn);
     itemList.appendChild(li);
+     //edit buton
+     var editBtn = document.createElement('button');
+     editBtn.className="btn btn-sm float-right edit";
+     editBtn.appendChild(document.createTextNode('edit'));
+     li.appendChild(editBtn);
+     itemList.appendChild(li);
 }
 function removeItem(e) {
    if(e.target.classList.contains('delete')){
@@ -30,6 +37,32 @@ function removeItem(e) {
     }
    }        
 }
+
+//filter search 
+
+function filterItems(e) {
+
+    //converttext to lowercase 
+    var text = e.target.value.toLowerCase();
+
+    //get list 
+    var items = itemList.getElementsByTagName("li");
+    // convert to array 
+
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;  
+        var descpdetail = item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 
+        || descpdetail.toLowerCase().indexOf(text) != -1) {
+            item.style.display ="block";
+        }else {
+            item.style.display= "none";
+        }
+
+    });
+
+}
+
 
 
 
